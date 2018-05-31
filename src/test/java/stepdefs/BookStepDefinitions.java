5afd6e49c662f51b73fc987a3ff7d7a8b90e8115
sergey <hodakovskiy@gmail.com>
2018-05-31 23:51:40 +0300

    test

---
+++ src/test/java/stepdefs/BookStepDefinitions.java
@@ -0,0 +1,30 @@
+package stepdefs;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class BookStepDefinitions {
+
+	private Response response;
+	private ValidatableResponse json;
+	private RequestSpecification request;
+
+	private String ENDPOINT_GET_BOOK_BY_ISBN = "https://www.googleapis.com/books/v1/volumes";
+
+
+}
+
+
